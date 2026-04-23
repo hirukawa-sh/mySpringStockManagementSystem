63a5756f895a8fd4bb14be15ceafc97dc65de7c6
hirukawa_sh <hirukawa_sh@yahoo.co.jp>
2026-04-23 20:39:17 +0900

    20260423_3

---
+++ src/main/java/com/example/DTO/SalesDto.java
@@ -0,0 +1,10 @@
+import com.example.demo.dto.SalesDetailDto;
+import java.time.LocalDateTime;
+
+public record SalesDto(
+    Long id,
+    String salesPerson,
+    Integer totalPrice,
+    LocalDateTime salesDate,
+    List<SalesDetailDto> details
+){}
