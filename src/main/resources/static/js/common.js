fc66cefcd9c29c1a5f54a38e37646e19f98f1b67
hirukawa_sh <hirukawa_sh@yahoo.co.jp>
2026-06-19 12:50:06 +0900

    20260619_9

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,21 +1,23 @@
-// テーブル行追加
-function addRow(elm) {
-  const table = elm.closest("table");
-  if (!table) {
-    console.error(`${elm}: table is not found`);
-    return;
+function common() {
+  // テーブル行追加
+  function addRow(query) {
+    const table = document.querySelector(query);
+    if (!table && table.tagName != "TABLE") {
+      console.error(`${elm}: table is not found`);
+      return;
+    }
+    table.insertRow();
   }
-  table.insertRow();
+  
+  // テーブル行削除
+  function deleteRow(elm) {
+    const row = elm.closest("tr");
+    if (!row) {
+      console.error(`${elm}: row is not found`);
+      return;
+    }
+    table.body[0].rows.remove(row);
+  }
 }
 
-// テーブル行削除
-function deleteRow(elm) {
-  const row = elm.cosest("tr");
-  if (!row) {
-    console.error(`${elm}: row is not found`);
-    return;
-  }
-  table.body[0].rows.remove(row);
-}
-
-export default *;
+export default common;
