d0081a009855e201312e455e8c9d2d4a12c71249
hirukawa_sh <hirukawa_sh@yahoo.co.jp>
2026-06-23 20:29:07 +0900

    20260623_1

---
+++ src/main/java/com/example/DTO/UserDto.java
@@ -0,0 +1,7 @@
+package com.example.dto;
+
+public record UserDto(
+    Long id,
+    String name,
+    String password,
+){}
---
+++ src/main/java/com/example/entities/User.java
@@ -0,0 +1,23 @@
+package com.example.entity;
+import lombok.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank(message = "ユーザー名は必須です")
+    @Size(max = 50, message = "50文字以内で入力してください")
+    private String name;
+    
+    @NotBlank(message = "パスワードは必須です")
+    @Size(max = 20, message = "20文字以内で入力してください")
+    private String password;
+    
+}
