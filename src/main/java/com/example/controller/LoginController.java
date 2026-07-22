e68e454a5cfde8c0783d64fc10c98669c637758e
hirukawa_sh <hirukawa_sh@yahoo.co.jp>
2026-07-23 07:36:14 +0900

    20260723_1

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jakarta.validation.Valid;
 
 import com.example.dto.*;
